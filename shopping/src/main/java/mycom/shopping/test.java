ccdc5e93ba69cdce915c13116bc829c13314b6ac
HQLD <1534443788@qq.com>
2019-08-26 23:50:49 +0800

    将web课设的商城保存到本地

--- shopping/src/main/java/mycom/shopping/test.java
+++ shopping/src/main/java/mycom/shopping/test.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 public class test {
     @Test
+//    测试查询邮箱地址是否正确
     public void test(){
         boolean b = CheckEmail.checkEmail("16@qq.com");
         System.out.println(b);
